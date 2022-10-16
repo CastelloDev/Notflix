f1a1e039a6af358b0ef8019ef10b076e647e9728
castellogovender <castello.dev@gmail.com>
2022-10-16 14:54:35 +0200

    Implemented zustand state management Including get images for topic api with mocks

---
+++ src/data/index.ts
@@ -0,0 +1,25 @@
+import threeDRenders from "./3d-renders.json";
+import architecture from "./architecture.json";
+import businessWork from "./business-work.json";
+import currentEvents from "./current-events.json";
+import experimental from "./experimental.json";
+import fashion from "./fashion.json";
+import foodDrink from "./food-drink.json";
+import nature from "./nature.json";
+import texturesPatterns from "./textures-patterns.json";
+import topics from "./topics.json";
+import wallpapers from "./wallpapers.json";
+
+export {
+  threeDRenders,
+  architecture,
+  businessWork,
+  currentEvents,
+  experimental,
+  fashion,
+  foodDrink,
+  nature,
+  texturesPatterns,
+  topics,
+  wallpapers,
+};
